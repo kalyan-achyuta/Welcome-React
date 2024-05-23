e422be2960b86a58d1aa97baddd5e9d0ac562881
Kalyan Achyuta <kalyanachyuta@gmail.com>
2024-05-23 12:38:39 +0530

    part2

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /*
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "i am h1 tag")
-  )
-);
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", {}, "i am h1 tag")
+        )
+    );
 */
 
 /*
@@ -26,3 +21,10 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 */
+
+const heading = React.createElement("h1", { id: "heading" }, "hello kalyan!!🎶");
+// In the above line we are creating an object by using createElement while rendering into the DOM it will convert 
+// into HTML element like in the below line...
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Welcome React 😎
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR (Hot Module Reloading)
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization - Most expensive things in web browsers
+- Minification - for production
+- Bundling
+- Compress
+- Content Hashing
+- Code Splitting
+- Differential Bundling - support of odler browsers
+- Diagnostics - Good Error View
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Build for dev and prod bundles
