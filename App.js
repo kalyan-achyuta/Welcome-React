ba2b70144dd9c86ed5126583281a329d4f98c77b
Kalyan Achyuta <kalyanachyuta@gmail.com>
2024-05-24 09:13:32 +0530

    part3

--- App.js
+++ App.js
@@ -1,30 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        React.createElement("h1", {}, "i am h1 tag")
-        )
-    );
-*/
+const Title =() => (
+<h1 className="head">Kalyan is here 😎</h1>
+);
 
-/*
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-]);
-*/
-
-const heading = React.createElement("h1", { id: "heading" }, "hello kalyan!!🎶");
-// In the above line we are creating an object by using createElement while rendering into the DOM it will convert 
-// into HTML element like in the below line...
+const Heading =() => (
+  <div>
+    {Title()}
+    <Title></Title>
+    <Title />
+    <h1 className="head">let's rock the react..🎆</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<Heading />);
